4b68191e12c5721d59d24b571304dd283aa1c1bb
calsmith <calsmith@umich.edu>
2022-09-20 23:40:11 -0400

    Able to HTTP GET from backend

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	//"fmt"
+	"log"
 	"net/http"
-	cors "github.com/rs/cors/wrapper/gin"
+
+	"io/ioutil"
+
 	"github.com/gin-gonic/gin"
+	cors "github.com/rs/cors/wrapper/gin"
 )
 
 //https://github.com/gin-gonic/gin
@@ -13,8 +17,20 @@ func main() {
 	router := gin.Default()
 	router.Use(cors.Default())
 	router.GET("/ping", func(c *gin.Context) {
+		resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		//We Read the response body on the line below.
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		//Convert the body to type string
+		sb := string(body)
+		log.Printf(sb)
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "pong" + sb,
 		})
 	})
 	router.Run(":8080")
